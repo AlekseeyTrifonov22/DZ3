bf5811a309ac5b4983613405ac619af2b7047e6e
AlekseeyTrifonov22 <bobikbobik2022@mail.ru>
2024-10-23 13:17:22 +0300

    Initial commit

---
+++ 5.py
@@ -0,0 +1,10 @@
+x1 = int(input("Количество часов:  "))
+if 0 <= x1 < 6:
+    print("Good Night")
+elif 6 <= x1 < 13:
+    print("Good Morning")
+elif 13 <= x1 < 17:
+    print("Good Day")
+elif 17 <= x1 < 24:
+    print("Good Evening")
+
